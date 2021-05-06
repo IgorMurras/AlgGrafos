b1d55ce90b3dba97945f20d9d62716ae7179cbc8
Igor <42788295+IgorMurras@users.noreply.github.com>
2021-05-05 21:51:10 -0300

    Add files via upload

--- Trabalho1/src/def/AlgGraf.java
+++ Trabalho1/src/def/AlgGraf.java
@@ -10,45 +10,61 @@ public class AlgGraf {
 	public static String filename; 
 	public static void main(String args[]) {
 		if(args.length !=1) {
-			System.out.println("Só é possivel entrar com o caminho do arquivo");
-		System.exit(-1) ;
-	}else {
+			System.out.println("So eh possivel entrar com o caminho do arquivo");
+			System.exit(-1) ;
+		}else {
 		filename = args[0];
-	}
-	Grafo grafo;
-	try {
-		File file = new File(filename);   			 	//creates a new file instance  
-		FileReader fr = new FileReader(file);  		 	//reads the file  
-		BufferedReader br = new BufferedReader(fr); 	//creates a buffering character input stream  
-		String line; 
+		}
+		Grafo grafo;
 		try {
-			BufferedReader contador = new BufferedReader(new FileReader(filename));
-			int n = 0;
-			while((line = contador.readLine()) != null) n++;
-			grafo = new Grafo(n);
-			contador.close();
-			System.out.println("n : "+n );
-			while((line = br.readLine()) != null) {
-				String[] lineList= line.split(" = ");
-				int v = Integer.parseInt( (lineList[0].replaceAll(" ", "")) );
-				String[] adjList= lineList[1].split(" ");
-				for(String wstr : adjList) {
-					int w = Integer.parseInt(wstr);
-					grafo.adicionaAresta(v-1, w-1); 
-					System.out.println("v: "+ v + " w: "+ w);
+			/*
+			 *	Nessa parte o arquivo de entrada eh lido e manipulado para a informacao do grafo ser utilizada de forma correta
+			 */
+			File file = new File(filename);   			 	// cria nova instancia de arquivo
+			FileReader fr = new FileReader(file);  		 	// leitura do arquivo
+			BufferedReader br = new BufferedReader(fr); 	// cria um buffer de caracteres
+			String line; 
+			try {
+				// Aqui primeiramente eh feito uma contagem do numero de linhas no arquivo, para ser utilizado como referencia depois na criacao grafo utilizado no algoritmo
+				BufferedReader contador = new BufferedReader(new FileReader(filename));
+				int n = 0;
+				while((line = contador.readLine()) != null) n++;
+				grafo = new Grafo(n);
+				contador.close();
+				
+				// Nessa parte eh feita a leitura de cada linha do arquivo de entrada
+				while((line = br.readLine()) != null) {
+					String[] lineList= line.split(" = ");						   	// aqui a entrada eh dividida em duas, no caso utilizando o sinal de '=' como referencia para dividir
+					
+					int v = Integer.parseInt( (lineList[0].replaceAll(" ", "")) ); 	/* aqui sao eliminados todos os espacos da primeira metade da linha (no caso o que estava antes do '='),
+																					 * para obter só o número do vertice e armazena em v */
+	
+					String[] adjList= lineList[1].split(" ");						/* aqui a segunda parte da entrada (que estava depois de '=') é dividida em varias,
+																					 * no caso utilizando os espaços " " como referencia para dividir, pegando o numero de cada vertice */ 
+					/*
+					 * Aqui cada vertice V (do lado esquerdo do sinal de '=') eh relacionado com seus respectivos vertices W (que estavam do lado direito do sinal de '=')  
+					 * Essa associação eh feita por meio do metodo adicionaAresta
+					 * Depois da associacao eh feito um print de cada v
+					 * */
+					for(String wstr : adjList) {
+						int w = Integer.parseInt(wstr);
+						grafo.adicionaAresta(v-1, w-1); 
+					}
 				}
-			}
-			br.close();
-			grafo.teste();	
-		} catch (IOException e) {
-			System.out.println("ERRO--IOException");
+				br.close();
+				/*
+				 * Aqui o teste eh rodado 
+				 */
+				grafo.teste();
+				
+			} catch (IOException e) {
+				System.out.println("ERRO--IOException");	// Erro na formatacao do arquivo
+				e.printStackTrace();
+			}	
+		} catch (FileNotFoundException e) {
+			System.out.println("ERRO--FileNotFound");		// Erro caso o arquivo nao seja encontrado
 			e.printStackTrace();
-		}	
-	} catch (FileNotFoundException e) {
-		System.out.println("ERRO--FileNotFound");
-		e.printStackTrace();
-    }	
-			
+		    }		
 	}
 	
 }
--- Trabalho1/src/def/Grafo.java
+++ Trabalho1/src/def/Grafo.java
@@ -6,113 +6,118 @@ import java.util.LinkedList;
 // Essa classe representa um grafo nao direcionado, usando representacao de lista de adjacencia
 class Grafo
 {
- private int V;   // Variavel respresentante do numero de vertices
+	 private int V;   // Variavel respresentante do numero de vertices
+	
+	 // Array de listas para representação da lista de adjacencias
+	 private LinkedList<Integer> lista_de_adjacencia[];
+	
+	 // Construtor
+	 Grafo(int v)
+	 {
+	     V = v;
+	     lista_de_adjacencia = new LinkedList[v];
+	     for (int i=0; i<v; ++i)
+	         lista_de_adjacencia[i] = new LinkedList();
+	 }
+	
+	 // Funcao para adicionar aresta no grafo
+	 void adicionaAresta(int v, int w)
+	 {
+	     lista_de_adjacencia[v].add(w);// Add w to v's list.
+	 }
+	
+	 // Funcao usada para busca em profundidade
+	 void buscaEmProfundidade(int v,boolean visitado[])
+	 {
+	 // Marca vertice como visitado
+	 visitado[v] = true;
+	 System.out.println(v+1);
+	 
+	 // Recorre para todos os vertices adjacentes a esse vertice 
+	     Iterator<Integer> i = lista_de_adjacencia[v].listIterator();
+	     while (i.hasNext())
+	     {
+	         int n = i.next();
+	         if (!visitado[n])
+	             buscaEmProfundidade(n, visitado);
+	     }
+	 }
+	 /*
+	  *	Metodo para checar se todos os vertices com grau diferente de zero sao conectados. Basicamente faz uma busca em profundidade.
+	  */
+	 boolean ehConexo()
+	 {
+		 // Marca todos os vertices como nao-visitados
+		 boolean visitado[] = new boolean[V];
+		 int i;
+		 for (i = 0; i < V; i++)
+		     visitado[i] = false;
+		
+		 // Acha um vertice com grau diferente de zero
+		 for (i = 0; i < V; i++)
+		     if (lista_de_adjacencia[i].size() != 0)
+		         break;
+		
+		 // Se nao tem arestas no grafo, retorna true
+		 if (i == V) {
+		     return true;
+		 }
+		 // Faz uma busca em profundidade com vertice de grau diferente de zero
+		 buscaEmProfundidade(i, visitado);
+		 
+		// Checa se todos os vertices de grau diferente de zero sao visitados
+	 	for (i = 0; i < V; i++) {
+	 		if (visitado[i] == false && lista_de_adjacencia[i].size() > 0) {
+	 			return false;
+	 		}
+	 	}
+	 	return true;
+	 }
 
- // Array  of lists for Adjacency List Representation
- private LinkedList<Integer> lista_de_adjacencia[];
-
- // Constructor
- Grafo(int v)
- {
-     V = v;
-     lista_de_adjacencia = new LinkedList[v];
-     for (int i=0; i<v; ++i)
-         lista_de_adjacencia[i] = new LinkedList();
- }
-
- //Function to add an edge into the graph
- void adicionaAresta(int v, int w)
- {
-     lista_de_adjacencia[v].add(w);// Add w to v's list.
- }
-
- // A function used by DFS
- void buscaEmProfundidade(int v,boolean visitado[])
- {
-     // Mark the current node as visited
-     visitado[v] = true;
-     System.out.println(v+1);
-     // Recur for all the vertices adjacent to this vertex
-     Iterator<Integer> i = lista_de_adjacencia[v].listIterator();
-     while (i.hasNext())
-     {
-         int n = i.next();
-         if (!visitado[n])
-             buscaEmProfundidade(n, visitado);
-     }
- }
-
- // Method to check if all non-zero degree vertices are
- // connected. It mainly does DFS traversal starting from
- boolean ehConexo()
- {
-     // Mark all the vertices as not visited
-     boolean visitado[] = new boolean[V];
-     int i;
-     for (i = 0; i < V; i++)
-         visitado[i] = false;
-
-     // Find a vertex with non-zero degree
-     for (i = 0; i < V; i++)
-         if (lista_de_adjacencia[i].size() != 0)
-             break;
-
-     // If there are no edges in the graph, return true
-     if (i == V)
-         return true;
-
-     // Start DFS traversal from a vertex with non-zero degree
-     buscaEmProfundidade(i, visitado);
-
-     // Check if all non-zero degree vertices are visited
-     for (i = 0; i < V; i++)
-        if (visitado[i] == false && lista_de_adjacencia[i].size() > 0)
-             return false;
-
-     return true;
- }
-
- /* The function returns one of the following values
-    0 --> If grpah is not Eulerian
-    1 --> If graph has an Euler path (Semi-Eulerian)
-    2 --> If graph has an Euler Circuit (Eulerian)  */
- int ehEuleriano()
- {
-     // Check if all non-zero degree vertices are connected
-     if (ehConexo() == false)
-         return 0;
-    
-     // Count vertices with odd degree
-     int impar = 0;
-     for (int i = 0; i < V; i++) {
-    	 System.out.println(lista_de_adjacencia[i]);
-         if (lista_de_adjacencia[i].size()%2!=0) {
-       
-             impar++;
-         }
-     }
-     
-     // 
-     System.out.println("Quantidade de vertices impares: " + impar);
-     if(impar==0 ) {
-    	 return 1;
-     }
-     else 
-    	 return 0;
- }
-
- // Function to run test cases
- void teste()
- {
-	 System.out.println("Numero de vertices V: " + V);
-     int res = ehEuleriano();
-     if (res == 1)
-    	 System.out.println("Há circuito euleriano");
-     else
-    	 System.out.println("Não há circuito euleriano");
-   
- }
-
- 
+	 /* 
+	  *  A funcao retorna 0 caso :
+	  *		- O grafo nao seja conexo
+	  *		- O numero de vertices de grau impar seja maior que 2
+	  *	A funcao retorna 1 caso :
+	  *		- Número de vértices ímpares for igual a 2
+	  *	A funcao retorna 2 caso:
+	  *		- Numero de vertices de grau impar for zero
+	  */
+	 int ehEuleriano()
+	    {
+		 	// Checa se todos os vertices de grau diferente de zero sao conexos, caso nao sejam, o grafo nao eh euleriano
+	        if (ehConexo() == false) {
+	            return 0;
+	        }
+	        // Conta o numero de vertices de grau impar
+	        int impar= 0;
+	        for (int i = 0; i < V; i++) {
+	            if (lista_de_adjacencia[i].size()%2!=0)
+	                impar++;
+	        }
+	        // Aqui eh mostrado o numero de vertices impares
+	        System.out.println("Quantidade de vertices impares: " + impar);
+	        
+	        // Caso o número de vertices de grau impar for maior que 3 o grafo nao eh euleriano
+	        if (impar > 2) {
+	            return 0;
+	        }
+	        // Caso o numero de vertices impares for igual a dois, existe um caminho euleriano, e 
+	        // caso o numero de vertices impares for igual a zero, existe um circuito euleriano
+	        return (impar==2)? 1 : 2;
+	    }
+	  
+	 	// Função para rodar os casos de teste
+	    void teste()
+	    {
+	        int resposta = ehEuleriano();
+	        if (resposta == 0)
+	            System.out.println("O grafo não é euleriano");
+	        else if (resposta == 1)
+	            System.out.println("O grafo tem um caminho euleriano");
+	        else
+	           System.out.println("O grafo tem um circuito euleriano");
+	    }
+	 
 }
 
